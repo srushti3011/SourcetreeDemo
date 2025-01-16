133210c0b0d98ff87f042fc5b9e0e90b6dd987ae
Srushti Sarju Engineer <srushti.engineer@simformsolutions.com>
2025-01-16 13:37:32 +0530

    Initial Commit

---
+++ XcodeDemoTests/XcodeDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeDemoTests.swift
+//  XcodeDemoTests
+//
+//  Created by Srushti Sarju Engineer on 16/01/25.
+//
+
+import Testing
+@testable import XcodeDemo
+
+struct XcodeDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
