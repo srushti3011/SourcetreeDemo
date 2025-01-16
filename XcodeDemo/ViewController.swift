8c2a3893ad0a4a68fd2f524a00e9c231be0741b8
Srushti Sarju Engineer <srushti.engineer@simformsolutions.com>
2025-01-16 14:03:27 +0530

    first commit done by me

--- XcodeDemo/ViewController.swift
+++ XcodeDemo/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //this is the comment added for testing push and commit
     }
 
 
